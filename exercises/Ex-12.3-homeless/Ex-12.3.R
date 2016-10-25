6eab075e026bf7bcfb6b6552d242366c36652650
Paul E. Johnson <pauljohn@ku.edu>
2016-10-25 14:45:46 -0500

    fix coding to match Stata

--- exercises/Ex-12.3-homeless/Ex-12.3.R
+++ exercises/Ex-12.3-homeless/Ex-12.3.R
@@ -2,8 +2,9 @@ library(foreign)
 library(survival)
 library(lme4)
 
-dat <- read.dta("http://www.stata-press.com/data/mlmus3/homeless.dta",
-  missing.type = T)
+dat <- read.dta("http://www.stata-press.com/data/mlmus3/homeless.dta")
+               ,
+  missing.type = TRUE)
 
 d1 <- read.dta("http://www.stata-press.com/data/mlmus3/travel1.dta",
 missing.type = T)
@@ -19,11 +20,13 @@ d1exp <- data.frame(name=rep(d1$traveler, each=4),group = rep(d1$alt, 4))
 # Not sure what NA is coded as in the .dta file. Does zero represent missing?
 summary(dat)
 
-#2
+                                        #2
+dat$mo_0 <- ifelse(dat$time == "0", 1, 0)
 dat$mo_6 <- ifelse(dat$time=="1", 1,0)
 dat$mo_12 <- ifelse(dat$time=="2", 1,0)
 dat$mo_24 <- ifelse(dat$time=="3", 1,0) 
 
+dat$int_0 <- dat$mo_0*dat$section8
 dat$int_6 <- dat$mo_6*dat$section8
 dat$int_12 <- dat$mo_12*dat$section8
 dat$int_24 <- dat$mo_24*dat$section8
@@ -32,8 +35,8 @@ dat$int_24 <- dat$mo_24*dat$section8
 library(nnet)
 
 dat$housing1 <- relevel(factor(dat$housing), ref= "0")
-m1 <- multinom(housing1 ~ section8 + mo_6 + mo_12 + mo_24 + int_6 +
-  int_12 + int_24, data = dat)
+m1 <- multinom(housing1 ~ section8 + mo_0 + mo_6 + mo_12 + int_0 + int_6 +
+  int_12, data = dat)
 summary(m1)
 # Not sure why results are different from Stata
 # They are very different. 
