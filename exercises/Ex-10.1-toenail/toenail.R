436d5a79f078d96e38ae9a8367e866baaab49a17
Paul E. Johnson <pauljohn@ku.edu>
2018-04-24 16:03:31 -0500

    toenail.R: clean up the random effect comparison

--- exercises/Ex-10.1-toenail/toenail.R
+++ exercises/Ex-10.1-toenail/toenail.R
@@ -15,22 +15,25 @@ library(rockchalk)
 summarize(dat)
 
 dat$treatmentn <- dat$treatment
-dat$treatment <- factor(dat$treatment, levels = c(0, 1), labels = c("itraconazole", "terbinafine"))
+dat$treatment <- factor(dat$treatment, levels = c(0, 1),
+                        labels = c("itraconazole", "terbinafine"))
 
 dat$outcomen <- dat$outcome
-dat$outcome <- factor(dat$outcome, levels = c(0, 1), labels = c("none/mild", "mod/severe"))
+dat$outcome <- factor(dat$outcome, levels = c(0, 1),
+                      labels = c("none/mild", "mod/severe"))
 dat$visit <- as.integer(dat$visit)
 
 dat$patient <- as.integer(dat$patient)
 
 summarize(dat)
 
- str(dat)
+str(dat)
 
-## Does R have something like xtdescribe
+## Does R have something like xtdescribe?
 
 ## What's not missing, compar to RHS p. 516
-table(by(dat$outcomen, list(dat$patient), function(x) paste(as.numeric(!is.na(x)), collapse = "")))
+table(by(dat$outcomen, list(dat$patient),
+         function(x) paste(as.numeric(!is.na(x)), collapse = "")))
 
 
 library(lattice)
@@ -43,7 +46,8 @@ t1pdfbad <- t1pdf[t1pdf$outcome == "mod/severe", ]
 barchart(Freq ~ visit | treatment,  t1pdfbad)
 
 
-m1 <- glm(outcome ~ treatment * month, data = dat, family = binomial(link = "logit"))
+m1 <- glm(outcome ~ treatment * month, data = dat,
+          family = binomial(link = "logit"))
 summary(m1)
 
 ## The odd's ratios
@@ -53,7 +57,7 @@ exp(coef(m1))
 library(sandwich)
 
 rse <- sqrt(diag(vcovHC(m1, type = "HC")))
-
+rse
 
 
 
@@ -64,11 +68,13 @@ dat$trt_month <- dat$treatmentn * dat$month
 
 ## See RHS 527
 m2 <- glmer(outcome ~ treatment * month + (1 | patient),
-            data = dat, family = binomial(link = "logit"), nAGQ = 30)
+            data = dat, family = binomial(link = "logit"),
+            nAGQ = 30)
 summary(m2)
 #1
 m3 <- glmer(outcome ~ treatment * month + (1|patient),
-             data = dat, family = binomial(link = "probit"), nAGQ=30)
+            data = dat, family = binomial(link = "probit"),
+            nAGQ=30)
 summary(m3)
 
 #2
@@ -79,12 +85,44 @@ summary(m3)
 #3
 re2 <- ranef(m2, condVar = T)
 re3 <- ranef(m3, condVar = T)
-ree2 <- unlist(unname(re2[[1]]))
-ree3 <- unlist(unname(re3[[1]]))
 
-c <- ree2[1]/ree3[1]
-approx_log <- ree3 * c
-plot(approx_log ~ ree2)
-diff <- approx_log - ree2
-summarize(diff)
+re2df <- as.data.frame(re2)
+re3df <- as.data.frame(re3)
 
+c <- re2df$condval/re3df$condval
+mean(c, na.rm = TRUE)
+## 1.8
+
+## regress the condval from one on other, find out
+## if there is proportional transformation
+m4 <- lm(re2df$condval ~ 0 + re3df$condval)
+summary(m4)
+
+## Don't suppress intercept, then what?
+m4a <- lm(re2df$condval ~ re3df$condval)
+summary(m4a)
+
+## Instructor manual reverses the comparison, it puts
+## probit in numerator, so
+c <- re3df$condval/re2df$condval
+mean(c, na.rm = TRUE)
+
+m5 <- lm(re3df$condval ~ 0 + re2df$condval)
+summary(m5)
+
+
+## Note the intsructor manual has to restrict
+## the sample to prevent use of more than one
+## respondent (re's return fill 1908 rows of the
+## data frame, but R is different.
+
+
+#4
+
+# Normal(0,1) is assumed in Probit
+
+# If logistic model assumes Logistic(0, 1), 1 is scale parameter,
+# then the variance of the logistic will be pi^2 / 3.
+
+# ratio of standard deviations is
+1 / sqrt(pi^2 / 3)
